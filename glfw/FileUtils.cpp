9635f70d1cfa756eaaca3469c2040a4a11ebf2dd
RyanBluth <ryanbluth93@gmail.com>
2014-07-28 11:02:52 -0700

    Found some better file reading code

--- glfw/FileUtils.cpp
+++ glfw/FileUtils.cpp
@@ -1,23 +1,17 @@
 #include "FileUtils.h"
 
-char*FileUtils::voxReadFile(const char* filename){
-	//Open file
-	FILE* fp = fopen(filename, READ);
-	//Move the file pointer to the end of the file ad determine the length
-	fseek(fp, 0, SEEK_END);
-	long fileLength = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	char* contents = new char[fileLength+1];
-	//zero out memory
-	for(int i = 0; i < fileLength+1; i++){
-		contents[i];
-	}
-	//Here we read the file
-	fread(contents, 1, fileLength, fp);
-	//Denote the end of the string
-	contents[fileLength-6] = '\0';
-	fclose(fp);
-
-	std::cout << contents << std::endl;
-	return contents;
+char*FileUtils::voxReadFile(const char* file){
+	
+	FILE *currentFile = fopen(file, READ);
+    fseek(currentFile, 0, SEEK_END);
+    int count = (int)ftell(currentFile);
+    
+    rewind(currentFile);
+    char *data = (char *)malloc(sizeof(char)*(count +1));
+    count = (int)fread(data, sizeof(char), count, currentFile);
+    data[count] = '\0';
+    
+    fclose(currentFile);
+    
+    return data;
 }
