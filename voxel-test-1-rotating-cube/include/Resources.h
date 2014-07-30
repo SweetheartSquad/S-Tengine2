987ccdc1720257b0cd19d1bfed6d48427d903daa
seleb <sean.s.leblanc@gmail.com>
2014-07-30 15:52:07 -0400

    added cinder projects
    You'll need to modify the project files to point to your cinder
    installation.
    
    voxel-test-1: the cube thing that shows the visual concept
    voxel-test-2: a broken drawing thingy
    voxel-test-3: a functional drawing thingy

---
+++ voxel-test-1-rotating-cube/include/Resources.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "cinder/CinderResources.h"
+#define RES_VERT_GLSL		CINDER_RESOURCE( ../resources/, vert.glsl, 128, GLSL )
+#define RES_FRAG_GLSL		CINDER_RESOURCE( ../resources/, frag.glsl, 129, GLSL )
+
+//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
