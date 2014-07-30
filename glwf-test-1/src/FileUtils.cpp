e6cbaf0b10c887586f9b87e3b25f9b773d1d1445
seleb <sean.s.leblanc@gmail.com>
2014-07-30 16:57:07 -0400

    added the dlls back in

--- glwf-test-1/src/FileUtils.cpp
+++ glwf-test-1/src/FileUtils.cpp
@@ -16,25 +16,8 @@ std::string FileUtils::voxReadFile(const char* filename){
 		file.close();
 	}else{
 		std::cout << "ERROR: file \"" << filename << "\" could not be opened for reading." << std::endl;
+		contents = "invalid file";
 	}
 
 	return contents;
-	
-	//Move the file pointer to the end of the file ad determine the length
-	/*fseek(fp, 0, SEEK_END);
-	long fileLength = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	char* contents = new char[fileLength+1];
-	//zero out memory
-	for(int i = 0; i < fileLength+1; i++){
-		contents[i];
-	}
-	//Here we read the file
-	fread(contents, 1, fileLength, fp);
-	//Denote the end of the string
-	contents[fileLength-6] = '\0';
-	fclose(fp);
-
-	std::cout << contents << std::endl;
-	return contents;*/
 }
