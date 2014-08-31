aaf2f6847f16ac1c64724555c5d87e0b9b9721e6
Emma Thurlow <ecthurlow@rogers.com>
2014-08-31 14:55:53 -0400

    Rough validation

--- Cinder/Source/skeleton-test/src/SkeletonData.cpp
+++ Cinder/Source/skeleton-test/src/SkeletonData.cpp
@@ -24,8 +24,8 @@ void SkeletonData::SaveSkeleton(vector<Joint *> &m_joints) {
 				if (b->parent != NULL) {
 					boneFile << "  \"parent_id\":" << b->parent->id << "," << endl;
 				}
-				boneFile << "  \"pos\":"<< "{\"x:\"" << b->pos.x << ", \"y\":" << b->pos.y << ", \"z:\"" << b->pos.z << "}," << endl;
-				boneFile << "  \"rotation\":"<< "{\"x\":" << b->rotation.v.x << ", \"y\":" << b->rotation.v.y << ", \"z\":" << b->rotation.v.z << ", \"w\":" << b->rotation.w << "}" << endl;
+				boneFile << "  \"pos\":"<< "{\"x:\"" << b->getPos().x << ", \"y\":" << b->getPos().y << ", \"z:\"" << b->getPos().z << "}," << endl;
+				boneFile << "  \"orientation\":"<< "{\"x\":" << b->orientation.v.x << ", \"y\":" << b->orientation.v.y << ", \"z\":" << b->orientation.v.z << ", \"w\":" << b->orientation.w << "}" << endl;
 				boneFile << " }";
 				if (b->id != m_joints.back()->id) {
 					boneFile << ",";
@@ -61,10 +61,25 @@ void SkeletonData::setDirectory(string directory) {
 }
 
 void SkeletonData::setFileName(string fileName) {
-	/*regex rgx = "[\/]";
-	if(fileName.find("/") =  string::npos){
-	}*/
-	this->fileName = fileName;
+	
+	regex rgx_name("[\\\\/<>\|\":\?\*]+");
+	regex rgx_ext("^.*\..*$");
+	//regex rgx_fmt("^.+\..*$");
+	try{
+		if(fileName.empty()){
+			throw exception("Filename is empty");
+		}
+		if(regex_match(fileName.begin(),fileName.end(),rgx_name)){
+			throw exception("Invalid filename: filename cannot contain \\/<>|\":?*");
+		}
+		if(!regex_match(fileName.begin(),fileName.end(),rgx_ext)){
+			app::console() << "Missing extension" << endl;
+			fileName.append(".txt");
+		}
+		this->fileName = fileName;
+	}catch(exception ex){
+		throw ex;
+	}
 }
 
 SkeletonData::~SkeletonData(void)
