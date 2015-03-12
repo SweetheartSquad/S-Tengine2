0053765a15f3fcb9681e0698b59071f091371f5a
RyanBluth <ryanbluth93@gmail.com>
2015-03-12 19:28:44 -0400

    Fixed controller

--- vox/Source/vox/src/AccelerometerParser.cpp
+++ vox/Source/vox/src/AccelerometerParser.cpp
@@ -30,50 +30,52 @@ void AccelerometerParser::addAccelerometer(Accelerometer* _accelerometer){
 void AccelerometerParser::update(Step* _step){
 	Arduino::update(_step);
 	if(IsConnected()) {
+		bool dataRead = false;
 		char buffer[LINE_SIZE];
 		if(firstPing) {
 			ClearCommError(this->hSerial, &this->errors, &this->status);
 		}
 		if(this->status.cbInQue == LINE_SIZE){
 			int numRead = ReadData(buffer, LINE_SIZE);
-			if(numRead > 0 && buffer[0] == ':'){
-				if(numRead >= LINE_SIZE){
-					firstPing = false;
-					for(unsigned long int i = 0; i < accelerometers.size(); ++i){
-						char * src = buffer + (i*9) + 1;
-						char x[4];
-						char y[4];
-						char z[4];
+			if(numRead == LINE_SIZE && buffer[0] == ':'){
+				dataRead = true;
+				firstPing = false;
+				for(unsigned long int i = 0; i < accelerometers.size(); ++i){
+					char * src = buffer + (i*9) + 1;
+					char x[4];
+					char y[4];
+					char z[4];
 						
-						x[0] = src[0];
-						x[1] = src[1];
-						x[2] = src[2];
-						x[3] = '\0';
+					x[0] = src[0];
+					x[1] = src[1];
+					x[2] = src[2];
+					x[3] = '\0';
 						
-						y[0] = src[3];
-						y[1] = src[4];
-						y[2] = src[5];
-						y[3] = '\0';
+					y[0] = src[3];
+					y[1] = src[4];
+					y[2] = src[5];
+					y[3] = '\0';
 						
-						z[0] = src[6];
-						z[1] = src[7];
-						z[2] = src[8];
-						z[3] = '\0';
+					z[0] = src[6];
+					z[1] = src[7];
+					z[2] = src[8];
+					z[3] = '\0';
 
-						accelerometers.at(i)->lx = accelerometers.at(i)->x;
-						accelerometers.at(i)->ly = accelerometers.at(i)->y;
-						accelerometers.at(i)->lz = accelerometers.at(i)->z;
-						accelerometers.at(i)->x = atoi(x);
-						accelerometers.at(i)->y = atoi(y);
-						accelerometers.at(i)->z = atoi(z);
-						accelerometers.at(i)->update(_step);
-					}
+					accelerometers.at(i)->lx = accelerometers.at(i)->x;
+					accelerometers.at(i)->ly = accelerometers.at(i)->y;
+					accelerometers.at(i)->lz = accelerometers.at(i)->z;
+					accelerometers.at(i)->x = atoi(x);
+					accelerometers.at(i)->y = atoi(y);
+					accelerometers.at(i)->z = atoi(z);
+					accelerometers.at(i)->update(_step);
 				}
-			}else {
-				PurgeComm(hSerial, PURGE_RXCLEAR);
-			} 
+			}
 		}else {
-			PurgeComm(hSerial, PURGE_RXCLEAR);	
+			if(this->status.cbInQue > 0){
+				char * buff = new char[this->status.cbInQue];
+				ReadData(buff, this->status.cbInQue);
+				delete buff;
+			}
 		}
 		char ping[1] = {'1'};
 		WriteData(ping, 1);
--- vox/Source/vox/src/PuppetCharacter.cpp
+++ vox/Source/vox/src/PuppetCharacter.cpp
@@ -231,13 +231,13 @@ void PuppetCharacter::jump(){
 		std::cout << "jump called for " << &torso << "\n";
 		float t = torso->body->GetAngle();
 		b2Vec2 p = torso->body->GetWorldPoint(b2Vec2(0, 1));
-		torso->applyLinearImpulse(250*(1-cos(t))*glm::sign(-t), 250*(cos(t)*0.5 + 0.5), p.x, p.y);
-		torso->applyLinearImpulseUp(250*(cos(t)*0.5 + 0.5));
+		//torso->applyLinearImpulse(250*(1-cos(t))*glm::sign(-t), 250*(cos(t)*0.5 + 0.5), p.x, p.y);
+		torso->applyLinearImpulseUp(175*(cos(t)*0.5 + 0.5));
 		if(torso->body->GetAngle() > 0){
 			//torso->applyLinearImpulseLeft(250*(1-cos(t)));
-			torso->body->SetLinearVelocity(b2Vec2(-250*(1-cos(t)), torso->body->GetLinearVelocity().y));
+			torso->body->SetLinearVelocity(b2Vec2(-25*(1-cos(t)), torso->body->GetLinearVelocity().y));
 		}else{
-			torso->body->SetLinearVelocity(b2Vec2(250*(1-cos(t)), torso->body->GetLinearVelocity().y));
+			torso->body->SetLinearVelocity(b2Vec2(25*(1-cos(t)), torso->body->GetLinearVelocity().y));
 		}
 	}
 	//canJump = false;
