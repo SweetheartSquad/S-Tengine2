8f723bad8741f3c6f72fd9135c7cfa23feb785a7
Emma Thurlow <ecthurlow@rogers.com>
2015-04-04 15:16:30 -0400

    Implementing structure particle destruction

--- vox/Source/vox/include/StructureBreakable.h
+++ vox/Source/vox/include/StructureBreakable.h
@@ -6,16 +6,24 @@
 
 class Box2DSprite;
 class Box2DWorld;
+class ParticleSystem;
 
 class StructureBreakable : public Structure{
 public:
 
 	float health;
 	float maxHealth;
+	bool dead;
+	bool scattering;
+	bool destroy;
+
+	ParticleSystem * particleSystem;
 
 	StructureBreakable(float _maxHealth, Box2DWorld * _world, int16 _categoryBits, int16 _maskBits = -1, int16 _groupIndex = 0);
 	
 	virtual void takeDamage(float _damage);
+
+	void update(Step* _step) override;
 
 	enum state_t{
 		kNORMAL,
