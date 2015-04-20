64b391d7e7801a5778b8286194d27298311a8656
seleb <sean.s.leblanc@gmail.com>
2015-04-20 06:51:49 -0400

    made directional light cutoff negative to flag it as not used

--- vox/Source/vox/src/DirectionalLight.cpp
+++ vox/Source/vox/src/DirectionalLight.cpp
@@ -2,7 +2,7 @@
 #include "Transform.h"
 
 DirectionalLight::DirectionalLight(glm::vec3 _direction, glm::vec3 _intensities, float _ambientCoefficient):
-	Light(LightType::kDIRECTIONAL_LIGHT, _direction, _intensities, _ambientCoefficient, 1.f, 0.f, transform),
+	Light(LightType::kDIRECTIONAL_LIGHT, _direction, _intensities, _ambientCoefficient, 1.f, -1.f, transform),
 	NodeChild(nullptr),
 	NodeTransformable(new Transform()),
 	isKeyLight(false)
