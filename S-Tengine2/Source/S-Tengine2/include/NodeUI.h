acffccfdd656329cf804441c1e9e7e3de30b46a3
seleb <sean.s.leblanc@gmail.com>
2015-05-15 20:48:55 -0400

    super basic UI thing

---
+++ S-Tengine2/Source/S-Tengine2/include/NodeUI.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <NodeBulletBody.h>
+#include <Entity.h>
+
+class Scene;
+
+class NodeUI : public virtual NodeBulletBody, public virtual Entity{
+private:
+	// Whether the button is under the mouse
+	bool isHovered;
+	// Whether the mouse clicked the button (and hasn't been released)
+	bool isDown;
+	// Whether the button is active (only affects RADIO and TOGGLE buttons)
+	bool isActive;
+	
+    // The function to be called when you are starting a click
+	void down();
+    // The function to be called when you complete a click
+	void up();
+    // The function to be called when you mouseover
+	void in();
+    // The function to be called when you mouseout
+	void out();
+public:
+	Scene * scene;
+
+	NodeUI(BulletWorld * _world, Scene * _scene);
+
+	virtual void update(Step * _step) override;
+	virtual void render(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions) override;
+	
+	// renders the UI element in it's "over" state
+	virtual void renderOver(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions);
+	// renders the UI element in it's "down" state
+	virtual void renderDown(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions);
+	// renders the UI element in it's "mousedown" state
+	virtual void renderActive(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions);
+	// called by the functions to render each individual state by default (so if you leave a state unimplemented, this will render instead)
+	virtual void renderDefault(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions);
+};
---
+++ S-Tengine2/Source/S-Tengine2/src/NodeUI.cpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <NodeUI.h>
+#include <Scene.h>
+#include <Camera.h>
+
+NodeUI::NodeUI(BulletWorld * _world, Scene * _scene) :
+	NodeBulletBody(_world),
+	scene(_scene),
+	isHovered(false),
+	isDown(false),
+	isActive(false)
+{
+
+}
+
+
+void NodeUI::down(){
+	isHovered = true;
+	isDown = true;
+	
+    // do event stuff
+}
+void NodeUI::up(){
+	isDown = false;
+	
+	// do event stuff
+}
+void NodeUI::in(){
+	isHovered = true;
+}
+void NodeUI::out(){
+	isHovered = false;
+}
+
+
+void NodeUI::update(Step * _step){
+	
+	float raylength = 1000;
+
+	Camera * cam = scene->activeCamera;
+		
+	glm::vec3 campos = cam->getWorldPos();
+	glm::vec3 camdir = cam->forwardVectorRotated;
+	btVector3 raystart(campos.x, campos.y, campos.z);
+	btVector3 raydir(camdir.x, camdir.y, camdir.z);
+	btVector3 rayend = raystart + raydir*raylength;
+	
+	/*btCollisionWorld::ClosestRayResultCallback RayCallback(raystart, rayend);
+	world->world->rayTest(raystart, rayend, RayCallback);
+	if(RayCallback.hasHit()){
+
+	}*/
+	btTransform rayfrom;
+	rayfrom.setIdentity(); rayfrom.setOrigin(raystart);
+	btTransform rayto;
+	rayto.setIdentity(); rayto.setOrigin(rayend);
+	btCollisionWorld::AllHitsRayResultCallback raycb(raystart, rayend);
+	world->world->rayTestSingle(rayfrom, rayto, body, shape, body->getWorldTransform(), raycb);
+
+	if(raycb.hasHit()){
+		if(!isHovered){
+			in();
+		}
+	}else{
+		if(isHovered){
+			out();
+		}
+	}
+
+	NodeBulletBody::update(_step);
+	Entity::update(_step);
+}
+
+void NodeUI::render(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions){
+	if(isDown){
+		renderDown(_matrixStack, _renderOptions);
+	}else if(isHovered){
+		renderOver(_matrixStack, _renderOptions);
+	}else if(isActive){
+		renderActive(_matrixStack, _renderOptions);
+	}else{
+		renderDefault(_matrixStack, _renderOptions);
+	}
+}
+
+void NodeUI::renderDown(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions){
+	std::cout << "down" << std::endl;
+	renderDefault(_matrixStack, _renderOptions);
+}
+
+void NodeUI::renderOver(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions){
+	std::cout << "over" << std::endl;
+	renderDefault(_matrixStack, _renderOptions);
+}
+
+void NodeUI::renderActive(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions){
+	std::cout << "active" << std::endl;
+	renderDefault(_matrixStack, _renderOptions);
+}
+
+void NodeUI::renderDefault(vox::MatrixStack * _matrixStack, RenderOptions * _renderOptions){
+	std::cout << "default" << std::endl;
+	Entity::render(_matrixStack, _renderOptions);
+}
