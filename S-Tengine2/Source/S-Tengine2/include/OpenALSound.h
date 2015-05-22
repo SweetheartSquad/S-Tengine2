d703fba53efbe3b44409d44c0e23dc0b42bfb800
seleb <sean.s.leblanc@gmail.com>
2015-05-22 09:40:14 -0400

    moved openal stuff to engine and set up project configurations

---
+++ S-Tengine2/Source/S-Tengine2/include/OpenALSound.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <node\NodeChild.h>
+#include <node\NodeUpdatable.h>
+#include <AL\alure.h>
+#include <iostream>
+
+//#define NUM_BUFS 32
+
+class OpenAL_Sound : public virtual NodeUpdatable, public virtual NodeChild{
+private:
+	static bool inited;
+
+	static void initOpenAL();
+	static void uninitOpenAL();
+public:
+    static ALCcontext * context;
+    static ALCdevice * device;
+	
+	// sets the global OpenAL listener position
+	static void setListenerPosition(glm::vec3 _position);
+	// sets the global OpenAL listener position
+	static void setListenerVelocity(glm::vec3 _velocity);
+	// sets the global OpenAL listener orientation
+	static void setListenerOrientation(glm::vec3 _forward, glm::vec3 _up);
+
+	ALuint sourceId;
+	ALuint bufferId;
+	alureStream * stream;
+	bool loop;
+
+	OpenAL_Sound(const char * _filename, bool _loop);
+	~OpenAL_Sound();
+
+	virtual void update(Step * _step) override;
+	
+	void setPosition(glm::vec3 _pos);
+	void setDirection(glm::vec3 _forward, glm::vec3 _up);
+};
+class OpenAL_Stream : public OpenAL_Sound{
+public:
+	OpenAL_Stream(const char * _filename);
+	~OpenAL_Stream();
+	void update(Step * _step) override;
+};
