e23afcc93fd9b30f9710e5f83df49d25aaa10147
seleb <sean.s.leblanc@gmail.com>
2015-06-27 18:05:32 -0400

    fixed destructor

--- S-Tengine2/Source/S-Tengine2/src/Box2DSuperSprite.cpp
+++ S-Tengine2/Source/S-Tengine2/src/Box2DSuperSprite.cpp
@@ -26,14 +26,6 @@ Box2DSuperSprite::Box2DSuperSprite(Box2DWorld * _world, int16 _categoryBits, int
 }
 
 Box2DSuperSprite::~Box2DSuperSprite(){
-	while(components.size() > 0){
-		delete *components.back();
-		/*try{
-			delete components.back();
-		}catch(std::exception){
-		}*/
-		components.pop_back();
-	}
 	components.clear();
 }
 
