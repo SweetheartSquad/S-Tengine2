cb32eb3bf363498f13aeaa027887467b0be60d66
seleb <sean.s.leblanc@gmail.com>
2016-01-11 14:02:25 -0500

    added a typedef for EventManager::Listener so you don't have to remember the function pointer stuff

--- S-Tengine2/Source/S-Tengine2/include/EventManager.h
+++ S-Tengine2/Source/S-Tengine2/include/EventManager.h
@@ -8,6 +8,8 @@
 #include <functional>
 #include <json/json.h>
 
+// 
+
 namespace sweet{
 
 class Event : public Node{
@@ -53,11 +55,13 @@ private:
 	// then calls handle on the parent managers
 	void handle(sweet::Event * _event);
 public:
+	// function pointer which takes a pointer to an event as an argument and returns void
+	typedef std::function<void (sweet::Event *)> Listener;
 	
 	// when an event is consumed, it will access listeners[event->key]
 	// and will call each function in the result, passing itself in as an argument
 	// listeners are handled in order from first added to last added
-	std::map<std::string, std::vector<std::function<void(sweet::Event *)>>> listeners;
+	std::map<std::string, std::vector<Listener>> listeners;
 
 	// triggers an event using the given object
 	void triggerEvent(sweet::Event * _event);
@@ -65,7 +69,8 @@ public:
 	// triggers an empty event object with the provided tag
 	void triggerEvent(std::string _tag);
 
-	void addEventListener(std::string _tag, std::function<void (sweet::Event *)> _listener );
+	// listeners are usually in the format [captures](sweet::Event * _event){ code; }
+	void addEventListener(std::string _tag, Listener _listener );
 
 	EventManager();
 	~EventManager();
