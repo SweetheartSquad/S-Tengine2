a8f324b0d4a208bc78d973b0cd75e9a6a6eed7bb
seleb <sean.s.leblanc@gmail.com>
2016-01-22 14:38:21 -0500

    fixed some issues with the colour class

--- S-Tengine2/Source/S-Tengine2/include/Colour.h
+++ S-Tengine2/Source/S-Tengine2/include/Colour.h
@@ -2,14 +2,14 @@
 
 #include <glm\glm.hpp>
 
-class Colour : public glm::uvec3{
+class Colour : public glm::ivec3{
 public:
 	// 255,255,255
 	Colour();
 	// 0-255 range
 	Colour(unsigned char _r, unsigned char _g, unsigned char _b);
 	// 0-255 range
-	Colour(glm::uvec3 _rgb);
+	Colour(glm::ivec3 _rgb);
 
 	// returns a darker shade of this colour
 	Colour darker();
@@ -18,16 +18,18 @@ public:
 
 	// converts to this colour to HSV, adds _hsv, then converts back to RGB and returns the result
 	// HSV is in range (0-360, 0-100, 0-100)
-	Colour hsvMod(glm::uvec3 _hsv);
+	Colour hsvMod(glm::ivec3 _hsv);
 	
 	// Converts this colour to HSV colour space
 	// HSV is in range (0-360, 0-100, 0-100)
-	static glm::uvec3 rgbToHsv(glm::uvec3 _rgb);
+	static glm::ivec3 rgbToHsv(glm::ivec3 _rgb);
 
 	// Converts this colour to HSV colour space
 	// HSV is in range (0-360, 0-100, 0-100)
-	static glm::uvec3 hsvToRgb(glm::uvec3 _hsv);
+	static glm::ivec3 hsvToRgb(glm::ivec3 _hsv);
 
-	static Colour getRandomInRgbRange(glm::uvec3 _min = glm::uvec3(0,0,0), glm::uvec3 _max = glm::uvec3(255,255,255));
-	static Colour getRandomFromRgbMean(glm::uvec3 _mean = glm::uvec3(128,128,128), glm::uvec3 _halfRange = glm::uvec3(0,0,0));
+	static Colour getRandomFromRgbRange(glm::ivec3 _min = glm::ivec3(0,0,0), glm::ivec3 _max = glm::ivec3(255,255,255));
+	static Colour getRandomFromRgbMean(glm::ivec3 _mean = glm::ivec3(128,128,128), glm::ivec3 _halfRange = glm::ivec3(0,0,0));
+	static Colour getRandomFromHsvRange(glm::ivec3 _min = glm::ivec3(0,0,0), glm::ivec3 _max = glm::ivec3(360,100,100));
+	static Colour getRandomFromHsvMean(glm::ivec3 _mean = glm::ivec3(180,50,50), glm::ivec3 _halfRange = glm::ivec3(0,0,0));
 };
--- S-Tengine2/Source/S-Tengine2/src/Colour.cpp
+++ S-Tengine2/Source/S-Tengine2/src/Colour.cpp
@@ -4,37 +4,41 @@
 #include <NumberUtils.h>
 
 Colour::Colour(unsigned char _r, unsigned char _g, unsigned char _b) :
-	glm::uvec3(_r, _g, _b)
+	glm::ivec3(_r, _g, _b)
 {
 }
 
-Colour::Colour(glm::uvec3 _rgb) :
-	glm::uvec3(_rgb)
+Colour::Colour(glm::ivec3 _rgb) :
+	glm::ivec3(_rgb)
 {
 }
 
 Colour::Colour() :
-	glm::uvec3(255, 255, 255)
+	glm::ivec3(255, 255, 255)
 {
 }
 
 Colour Colour::brighter(){
-	return Colour(*this + glm::uvec3(10));
+	return Colour(*this + glm::ivec3(10));
 }
 Colour Colour::darker(){
-	return Colour(*this - glm::uvec3(10));
+	return Colour(*this - glm::ivec3(10));
 }
 
-Colour Colour::hsvMod(glm::uvec3 _hsvDelta){
-	return hsvToRgb(rgbToHsv(*this) + _hsvDelta);
+Colour Colour::hsvMod(glm::ivec3 _hsvDelta){
+	glm::ivec3 hsv = rgbToHsv(*this) + _hsvDelta;
+	hsv.x = glm::mod((float)hsv.x, 360.f);
+	hsv.y = glm::min(100, glm::max(0, hsv.y));
+	hsv.z = glm::min(100, glm::max(0, hsv.z));
+	return Colour(hsvToRgb(hsv));
 }
 	
-glm::uvec3 Colour::rgbToHsv(glm::uvec3 _rgb){
+glm::ivec3 Colour::rgbToHsv(glm::ivec3 _rgb){
 	glm::vec3 n = glm::vec3(_rgb) / 255.f;
 	float cMax = glm::max(n.r, glm::max(n.g, n.b));
 	float cMin = glm::min(n.r, glm::min(n.g, n.b));
 	float delta = cMax - cMin;
-	glm::uvec3 hsv;
+	glm::ivec3 hsv;
 
 	// hue calculation
 	if(delta < FLT_EPSILON){
@@ -56,7 +60,7 @@ glm::uvec3 Colour::rgbToHsv(glm::uvec3 _rgb){
 
 	return hsv;
 }
-glm::uvec3 Colour::hsvToRgb(glm::uvec3 _hsv){
+glm::ivec3 Colour::hsvToRgb(glm::ivec3 _hsv){
 	float c = (_hsv.z * _hsv.y)/10000.f;
 	float x = c * (1 - glm::abs(fmod(_hsv.x / 60.f, 2) - 1));
 	float m = _hsv.z/100.f - c;
@@ -77,13 +81,21 @@ glm::uvec3 Colour::hsvToRgb(glm::uvec3 _hsv){
 		n = glm::vec3(c, 0, x);
 	}
 	
-	return glm::uvec3((n + glm::vec3(m)) * 255.f);
+	return glm::ivec3((n + glm::vec3(m)) * 255.f);
 }
 
-Colour Colour::getRandomInRgbRange(glm::uvec3 _min, glm::uvec3 _max){
+Colour Colour::getRandomFromRgbRange(glm::ivec3 _min, glm::ivec3 _max){
 	return Colour(sweet::NumberUtils::randomInt(_min.r, _max.r), sweet::NumberUtils::randomInt(_min.g, _max.g), sweet::NumberUtils::randomInt(_min.b, _max.b));
 }
 
-Colour Colour::getRandomFromRgbMean(glm::uvec3 _mean, glm::uvec3 _halfRange){
-	return Colour(_mean + glm::uvec3(sweet::NumberUtils::randomInt(-_halfRange.r, _halfRange.r), sweet::NumberUtils::randomInt(-_halfRange.g, _halfRange.g), sweet::NumberUtils::randomInt(-_halfRange.b, _halfRange.b)));
+Colour Colour::getRandomFromRgbMean(glm::ivec3 _mean, glm::ivec3 _halfRange){
+	return Colour(_mean + glm::ivec3(sweet::NumberUtils::randomInt(-_halfRange.r, _halfRange.r), sweet::NumberUtils::randomInt(-_halfRange.g, _halfRange.g), sweet::NumberUtils::randomInt(-_halfRange.b, _halfRange.b)));
+}
+
+Colour Colour::getRandomFromHsvRange(glm::ivec3 _min, glm::ivec3 _max){
+	return hsvToRgb(glm::ivec3(sweet::NumberUtils::randomInt(_min.r, _max.r), sweet::NumberUtils::randomInt(_min.g, _max.g), sweet::NumberUtils::randomInt(_min.b, _max.b)));
+}
+
+Colour Colour::getRandomFromHsvMean(glm::ivec3 _mean, glm::ivec3 _halfRange){
+	return hsvToRgb(glm::ivec3(_mean + glm::ivec3(sweet::NumberUtils::randomInt(-_halfRange.r, _halfRange.r), sweet::NumberUtils::randomInt(-_halfRange.g, _halfRange.g), sweet::NumberUtils::randomInt(-_halfRange.b, _halfRange.b))));
 }
