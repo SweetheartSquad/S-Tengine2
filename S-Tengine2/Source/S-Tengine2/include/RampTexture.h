c1788e9e349a01ed761102337ee25ad46a44da48
seleb <sean.s.leblanc@gmail.com>
2016-01-22 16:57:09 -0500

    minor ramp update

--- S-Tengine2/Source/S-Tengine2/include/RampTexture.h
+++ S-Tengine2/Source/S-Tengine2/include/RampTexture.h
@@ -4,5 +4,9 @@
 
 class RampTexture : public ProgrammaticTexture {
 public:
+	glm::vec3 start, end;
+	unsigned long int levels;
 	RampTexture(glm::vec3 _start, glm::vec3 _end, unsigned long int _levels, bool autoRelease=true);
+
+	void setRamp(glm::vec3 _start, glm::vec3 _end, unsigned long _levels);
 };
--- S-Tengine2/Source/S-Tengine2/src/RampTexture.cpp
+++ S-Tengine2/Source/S-Tengine2/src/RampTexture.cpp
@@ -9,15 +9,22 @@ RampTexture::RampTexture(glm::vec3 _start, glm::vec3 _end, unsigned long _levels
 {
 	allocate(_levels, 1, 4);
 
+	setRamp(_start, _end, _levels);
+}
+
+
+void RampTexture::setRamp(glm::vec3 _start, glm::vec3 _end, unsigned long _levels){
+	start = _start;
+	end = _start;
+	levels = _levels;
 	for(unsigned long int i = 0; i < width; ++i){
-		
-		unsigned char * pixel = sweet::TextureUtils::getPixelPointer(this, i, 0, 0);
-		
 		double pStep = i/ static_cast<double>(width);
-		
-		*(pixel+0) = (_start.r * (1 - pStep ) + _end.r * (pStep)) * 255.0;
-		*(pixel+1) = (_start.g * (1 - pStep ) + _end.g * (pStep)) * 255.0;
-		*(pixel+2) = (_start.b * (1 - pStep ) + _end.b * (pStep)) * 255.0;
-		*(pixel+3) = 255;
+		sweet::TextureUtils::setPixel(this, i, 0, glm::uvec4(
+			(_start.r * (1 - pStep ) + _end.r * (pStep)) * 255.0,
+			(_start.g * (1 - pStep ) + _end.g * (pStep)) * 255.0,
+			(_start.b * (1 - pStep ) + _end.b * (pStep)) * 255.0,
+			255
+		));
+		unsigned char * pixel = sweet::TextureUtils::getPixelPointer(this, i, 0, 0);
 	}
 }
