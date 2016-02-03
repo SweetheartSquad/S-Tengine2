587ea1c8a4d85275ed0629d72a34c6150c0c9497
seleb <sean.s.leblanc@gmail.com>
2016-02-03 14:33:36 -0500

    adding BulletHeightField
    texture is used to create the terrain collider, and terrain collider
    verts are used to create mesh

--- S-Tengine2/Source/S-Tengine2/include/BulletHeightField.h
+++ S-Tengine2/Source/S-Tengine2/include/BulletHeightField.h
@@ -4,6 +4,7 @@
 #include <BulletCollision\CollisionShapes\btHeightfieldTerrainShape.h>
 
 class Texture;
+class QuadMesh;
 
 // since a height field doesn't copy the data used to create it,
 // we need to make sure that we don't accidentally delete the
@@ -17,5 +18,17 @@ public:
 	// _upAxis = 0, 1, or 2, indicating x, y, and z axis respectively
 	BulletHeightFieldShape(Texture * _heightMap, glm::vec3 _scale = glm::vec3(1.f), unsigned long int _upAxis = 1);
 	~BulletHeightFieldShape();
+
+	// copies the bullet collider vertices into a new mesh
+	// NOTE: you have to manage the memory for this mesh on your own
+	TriMesh * getMesh();
 };
 
+class BulletHeightField : public BulletMeshEntity{
+private:
+	Texture * heightMap;
+public:
+	// NOTE: a BulletHeightField calls setColliderAsHeightMap in its constructor, so you don't need to construct the shape yourself
+	BulletHeightField(BulletWorld * _world, Texture * _heightMap, Shader * _shader, glm::vec3 _scale = glm::vec3(1.f), unsigned long int _upAxis = 1);
+	~BulletHeightField();
+};
--- S-Tengine2/Source/S-Tengine2/src/BulletHeightField.cpp
+++ S-Tengine2/Source/S-Tengine2/src/BulletHeightField.cpp
@@ -3,15 +3,63 @@
 #include <BulletHeightField.h>
 #include <Texture.h>
 #include <TextureUtils.h>
+#include <MeshInterface.h>
 
 BulletHeightFieldShape::BulletHeightFieldShape(Texture * _heightMap, glm::vec3 _scale, unsigned long int _upAxis) :
 	heightMap(_heightMap),
 	btHeightfieldTerrainShape(_heightMap->width, _heightMap->height, _heightMap->data, (_upAxis == 0 ? _scale.x : (_upAxis == 1 ? _scale.y : _scale.z)), sweet::TextureUtils::getMin(_heightMap), sweet::TextureUtils::getMax(_heightMap), _upAxis, PHY_ScalarType::PHY_UCHAR, false)
 {
-	++_heightMap->referenceCount;
+	++heightMap->referenceCount;
 	setLocalScaling(btVector3(_upAxis == 0 ? 1 : _scale.x, _upAxis == 1 ? 1 : _scale.y, _upAxis == 2 ? 1 : _scale.z));
 }
 
 BulletHeightFieldShape::~BulletHeightFieldShape(){
+	heightMap->decrementAndDelete();
+}
+
+TriMesh * BulletHeightFieldShape::getMesh(){
+	TriMesh * res = new TriMesh(true);
+	for(unsigned long int x = 1; x < heightMap->width; ++x){
+		for(unsigned long int y = 1; y < heightMap->height; ++y){
+			// copy verts
+			btVector3 v;
+			getVertex(x, y, v);
+			res->vertices.push_back(Vertex(v.x(), v.y(), v.z()));
+			getVertex(x-1, y, v);
+			res->vertices.push_back(Vertex(v.x(), v.y(), v.z()));
+			getVertex(x-1, y-1, v);
+			res->vertices.push_back(Vertex(v.x(), v.y(), v.z()));
+			getVertex(x, y-1, v);
+			res->vertices.push_back(Vertex(v.x(), v.y(), v.z()));
+			
+			// set uvs
+			res->setUV(res->vertices.size() - 1, (float)x/heightMap->width, (float)(y-1)/heightMap->height);
+			res->setUV(res->vertices.size() - 2, (float)(x-1)/heightMap->width, (float)(y-1)/heightMap->height);
+			res->setUV(res->vertices.size() - 3, (float)(x-1)/heightMap->width, (float)y/heightMap->height);
+			res->setUV(res->vertices.size() - 4, (float)x/heightMap->width, (float)y/heightMap->height);
+			
+			// connect verts
+			res->pushTri(res->vertices.size() - 4, res->vertices.size() - 3, res->vertices.size() - 2);
+			res->pushTri(res->vertices.size() - 4, res->vertices.size() - 2, res->vertices.size() - 1);
+		}
+	}
+	return res;
+}
+
+BulletHeightField::BulletHeightField(BulletWorld * _world, Texture * _heightMap, Shader * _shader, glm::vec3 _scale, unsigned long int _upAxis) :
+	BulletMeshEntity(_world, new TriMesh(true), _shader),
+	heightMap(_heightMap)
+{
+	++heightMap->referenceCount;
+
+	setColliderAsHeightMap(_heightMap, _scale, _upAxis);
+
+	BulletHeightFieldShape * s = dynamic_cast<BulletHeightFieldShape *>(shape);
+	TriMesh * temp = s->getMesh();
+	mesh->insertVertices(temp);
+	delete temp;
+}
+
+BulletHeightField::~BulletHeightField(){
 	heightMap->decrementAndDelete();
 }
