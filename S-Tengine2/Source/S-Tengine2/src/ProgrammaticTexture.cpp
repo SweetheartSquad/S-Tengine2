a73c9729fba98aceb034182d8ca904c85ca3c88b
seleb <sean.s.leblanc@gmail.com>
2016-02-03 18:54:25 -0500

    added noise and perlin noise textures

--- S-Tengine2/Source/S-Tengine2/src/ProgrammaticTexture.cpp
+++ S-Tengine2/Source/S-Tengine2/src/ProgrammaticTexture.cpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <ProgrammaticTexture.h>
+#include <NumberUtils.h>
+#include <TextureUtils.h>
+
 #include <assert.h>
 
 ProgrammaticTexture::ProgrammaticTexture(unsigned char * _data, bool _autoRelease, bool _useMipmaps) :
@@ -35,4 +38,43 @@ void ProgrammaticTexture::allocate(unsigned long int _width, unsigned long int _
 	numBytes = numPixels * channels;
 
 	data = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * numBytes));
+}
+
+
+NoiseTexture::NoiseTexture(unsigned long int _width, unsigned long int _height, bool _autoRelease, bool _useMipmaps) :
+	ProgrammaticTexture(nullptr, _autoRelease, _useMipmaps),
+	NodeResource(_autoRelease),
+	maxVal(255),
+	minVal(0)
+{
+	allocate(_width, _height, 1);
+	setNoise();
+}
+
+void NoiseTexture::setNoise(){
+	for(unsigned long int i = 0; i < numBytes; ++i){
+		data[i] = sweet::NumberUtils::randomInt(minVal, maxVal);
+	}
+}
+
+
+
+
+PerlinNoiseTexture::PerlinNoiseTexture(unsigned long int _width, unsigned long int _height, bool _autoRelease, bool _useMipmaps) :
+	ProgrammaticTexture(nullptr, _autoRelease, _useMipmaps),
+	NodeResource(_autoRelease),
+	amplitude(255),
+	offset(0),
+	frequency(1.f)
+{
+	allocate(_width, _height, 1);
+	setNoise(0);
+}
+
+void PerlinNoiseTexture::setNoise(float _time){
+	for(unsigned long int y = 0; y < height; ++y){
+	for(unsigned long int x = 0; x < width; ++x){
+		sweet::TextureUtils::setPixel(this, x, y, sweet::NumberUtils::pNoise( ((float)x)/(width) * frequency.x, ((float)y)/(height) * frequency.y, _time * frequency.z )*255.f + offset);
+	}
+	}
 }
